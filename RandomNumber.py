d793910458139e1f815e7a468f1a71290fe34b0b
xokc4 <42719177+xokc4@users.noreply.github.com>
2024-05-26 19:37:36 +0400

    Initial commit of my Python project

---
+++ RandomNumber.py
@@ -0,0 +1,10 @@
+def is_attacking(q1, q2):
+    return q1[0] == q2[0] or q1[1] == q2[1] or abs(q1[0] - q2[0]) == abs(q1[1] - q2[1])
+def check_queens(queens):
+    for i in range(len(queens)):
+        for j in range(i + 1, len(queens)):
+            if is_attacking(queens[i], queens[j]):
+                return True
+    return False
+queens = [(1, 1), (2, 3), (3, 5), (4, 7), (5, 2), (6, 4), (7, 6), (8, 8)]
+print(check_queens(queens))
---
+++ main.py
@@ -0,0 +1,61 @@
+import warnings
+
+warnings.filterwarnings('ignore')
+import csv
+import json
+import random
+
+
+def generate_csv_file(file_name, rows):
+    with open(file_name, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        for _ in range(rows):
+            row = [random.randint(1, 1000) for _ in range(3)]
+            writer.writerow(row)
+
+
+def find_roots(a, b, c):
+    discriminant = b ** 2 - 4 * a * c
+    if discriminant < 0:
+        return None
+    elif discriminant == 0:
+        return -b / (2 * a)
+    else:
+        root1 = (-b + discriminant ** 0.5) / (2 * a)
+        root2 = (-b - discriminant ** 0.5) / (2 * a)
+        return root1, root2
+
+
+def save_to_json(func):
+    def wrapper(file_name):
+        with open(file_name, newline='') as csvfile:
+            reader = csv.reader(csvfile)
+            data = []
+            for row in reader:
+                a, b, c = map(int, row)
+                result = func(a, b, c)
+                data.append({"parameters": [a, b, c], "result": result})
+
+        with open("results.json", 'w') as f:
+            json.dump(data, f, indent=4)
+
+    return wrapper
+
+
+@save_to_json
+def find_roots_with_saving(a, b, c):
+    return find_roots(a, b, c)
+
+
+generate_csv_file("input_data.csv", 101)
+find_roots("input_data.csv")
+
+with open("results.json", 'r') as f:
+    data = json.load(f)
+
+if 100<=len(data)<=1000:
+    print(True)
+else:
+    print(f"Количество строк в файле не находится в диапазоне от 100 до 1000.")
+
+print(len(data)==101)
